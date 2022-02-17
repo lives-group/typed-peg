49b7b8fca47e8add823999dfc968aba75ed959c1
Rodrigo Ribeiro <rodrigogribeiro@gmail.com>
2022-02-17 12:37:47 -0300

    Project re-structuring

--- info.rkt
+++ info.rkt
@@ -1,9 +1,10 @@
 #lang info
 (define collection "typed-peg")
 (define deps '("base"
-               "pprint"))
+               "pprint"
+               "peg-gen"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/peg.scrbl" ())))
+(define scribblings '(("scribblings/typed-peg.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(rodrigo))
