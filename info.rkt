c11099eeb0d2661b094b9ab65fe11ed655a079a3
Rodrigo Ribeiro <rodrigogribeiro@gmail.com>
2022-02-18 10:25:16 -0300

    Trying fixes on CI

--- info.rkt
+++ info.rkt
@@ -3,7 +3,11 @@
 (define deps '("base"
                "pprint"
                "peg-gen"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("pprint"
+                     "peg-gen"
+                     "scribble-lib"
+                     "racket-doc"
+                     "rackunit-lib"))
 (define scribblings '(("scribblings/typed-peg.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
